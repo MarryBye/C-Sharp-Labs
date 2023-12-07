81e84bef8359b159084661e9cd88849eec1edeb4
xMarryByex <vlukianov04@gmail.com>
2023-12-08 00:11:29 +0200

    Fix

--- Lab6/Lab6/Program.cs
+++ Lab6/Lab6/Program.cs
@@ -1,23 +1,32 @@
 ﻿using Lab6;
 
-Console.WriteLine("Число 1: ");
-string x1 = Console.ReadLine();
-
-Console.WriteLine("Число 2: ");
-string x2 = Console.ReadLine();
-
-Console.WriteLine("Система 1 числа: ");
-int s1 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Система 2 числа: ");
-int s2 = Convert.ToInt32(Console.ReadLine());
-
-DigitsTransformer digit1 = new DigitsTransformer(x1, s1);
-DigitsTransformer digit2 = new DigitsTransformer(x2, s2);
-
-if (digit1.MakeNormal() == digit2.MakeNormal()) {
-    Console.WriteLine($"TRUE | {digit1.Digit()}, {digit2.Digit()}");
-} else
+while (true)
 {
-    Console.WriteLine($"FALSE | {digit1.Digit()}, {digit2.Digit()}");
+    try
+    {
+        Console.WriteLine("Число 1: ");
+        string x1 = Console.ReadLine();
+
+        Console.WriteLine("Число 2: ");
+        string x2 = Console.ReadLine();
+
+        Console.WriteLine("Система 1 числа: ");
+        int s1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Система 2 числа: ");
+        int s2 = Convert.ToInt32(Console.ReadLine());
+
+        DigitsTransformer digit1 = new DigitsTransformer(x1, s1);
+        DigitsTransformer digit2 = new DigitsTransformer(x2, s2);
+
+        if (digit1.MakeNormal() == digit2.MakeNormal())
+        {
+            Console.WriteLine($"TRUE | {digit1.Digit()}, {digit2.Digit()}");
+        }
+        else
+        {
+            Console.WriteLine($"FALSE | {digit1.Digit()}, {digit2.Digit()}");
+        }
+
+    } catch { Console.WriteLine("Случилась ошибка! Введите корректные данные!"); }
 }
